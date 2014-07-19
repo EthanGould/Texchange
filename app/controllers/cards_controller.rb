19c05b730ac652b8e2f4175e947d9b82a5ad3c0c
Ethan Gould <ethan.g.907@gmail.com>
2014-07-19 12:46:24 -0400

    built form for new card + card show page

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -69,6 +69,6 @@ class CardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def card_params
-      params[:card]
+      params.require(:card).permit(:word)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+card1 = Card.create(word: 'apple', definition: 'yummy fruit, usually red or green', language: 'English')
+card2 = Card.create(word: '苹果', definition: '一种常见的水果', language: 'Chinese')
